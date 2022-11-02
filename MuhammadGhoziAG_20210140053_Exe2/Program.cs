4b6f79362b592012b43130225a31f2ecbe5150ad
Ghozi1 <mghozighifar04@gmail.com>
2022-11-02 15:45:42 +0700

    menginput array dengan nama ouzi beserta nilai max

--- MuhammadGhoziAG_20210140053_Exe2/Program.cs
+++ MuhammadGhoziAG_20210140053_Exe2/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace MuhammadGhoziAG_20210140053_Exe2
 {
-    internal class Program
+    class Program
     {
+        // Deklarasi array int dengan ukuran maksimal 28
+        private int[] ouzi = new int[28];
+
+ 
+         
         static void Main(string[] args)
         {
+
         }
     }
 }
