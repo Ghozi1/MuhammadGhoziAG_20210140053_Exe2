b05b63cb7102088c3bc5bc52c47f39ec04a25f0d
Ghozi1 <mghozighifar04@gmail.com>
2022-11-02 16:51:01 +0700

    input

--- MuhammadGhoziAG_20210140053_Exe2/Program.cs
+++ MuhammadGhoziAG_20210140053_Exe2/Program.cs
@@ -68,17 +68,19 @@ namespace MuhammadGhoziAG_20210140053_Exe2
         {
 
             // Merges two subarrays of []arr.
-            // First subarray is arr[l..m]
-            // Second subarray is arr[m+1..r]
-            void merge(int[] arr, int l, int m, int r)
+            void merge(int[] ouzi, int l, int m, int r)
             {
                 // Find sizes of two
                 // subarrays to be merged
                 int n1 = m - l + 1;
                 int n2 = r - m;
 
-                
+                // Create temp arrays
+                int[] L = new int[n1];
+                int[] R = new int[n2];
+                int i, j
             }
+
         }    
 
         //function
