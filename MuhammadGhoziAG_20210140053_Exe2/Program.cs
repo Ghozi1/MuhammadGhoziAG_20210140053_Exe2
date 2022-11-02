40b27affacdc329f46ccb20db463498fa70407f2
Ghozi1 <mghozighifar04@gmail.com>
2022-11-02 15:48:23 +0700

    membuat method untuk menginput

--- MuhammadGhoziAG_20210140053_Exe2/Program.cs
+++ MuhammadGhoziAG_20210140053_Exe2/Program.cs
@@ -14,8 +14,32 @@ namespace MuhammadGhoziAG_20210140053_Exe2
         // Deklarasi variable yang digunakan untuk menyimpan nilai data pada array
         private int n;
 
-        
-         
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array:");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine
+                        ("\nArray should have minimum 1 and maximum 20 elements. \n");
+            }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" Enter array elements");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                ouzi[i] = Int32.Parse(s1);
+            }
+        }
+
         static void Main(string[] args)
         {
 
